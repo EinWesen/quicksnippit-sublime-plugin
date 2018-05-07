807ee640e6f166c678f0d78b49d247ef99df508f
EinWesen <einwesen@gmail.com>
2018-05-07 22:55:55 +0200

    Rework to use inputhandlers

--- QuickSnippetCommands.py
+++ QuickSnippetCommands.py
@@ -15,19 +15,30 @@ def is_str_empty(s):
 		return False
 #end def
 
+def is_str_not_empty(s):
+	try:
+		return s.strip() != ""
+	except:
+		return True
+#end def
+
 # ******* END SHARED FUNCTIONS *******
 
 class InsertSelectionsplitSnippetCommand(sublime_plugin.TextCommand):	
 
-	def is_str_not_empty(self, s):
-		try:
-			return s.strip() != ""
-		except:
-			return True
-	#end def
+	class _DelimiterInputHandler(sublime_plugin.TextInputHandler):
+		def __init__(self): pass			
+		def name(self): return "delimiter"
+		def description(self, text): return text		
+		def placeholder(self): return get_quicksnippets_setting("default_delimiter", "|")
+		def preview(self, text):
+			if is_str_empty(text): text=get_quicksnippets_setting("default_delimiter", "|")
+			return "Choose the delimiter! Example data for current input: A{0}B{0}C".format(text)
+		#end def
+	#end sub class
 
 	def run(self, edit, delimiter, contents=None, name=None, variable=None, **args):
-		if is_str_empty(contents) and self.is_str_not_empty(name):
+		if is_str_empty(contents) and is_str_not_empty(name):
 			try:
 				contents = minidom.parseString(sublime.load_resource(name)).getElementsByTagName('content')[0].firstChild.data		
 			except (IOError, ValueError) as e:
@@ -36,7 +47,7 @@ class InsertSelectionsplitSnippetCommand(sublime_plugin.TextCommand):
 			#end try
 		#end if empty contents 
 
-		if self.is_str_not_empty(contents) and self.is_str_not_empty(delimiter): # if we finally have content
+		if is_str_not_empty(contents) and is_str_not_empty(delimiter): # if we finally have content
 
 			#apply some defaults
 			if is_str_empty(variable): variable=get_quicksnippets_setting("default_variable", "SEL_SPLIT")
@@ -57,41 +68,18 @@ class InsertSelectionsplitSnippetCommand(sublime_plugin.TextCommand):
 
 		#end if	contents not empty 
 	#end def
-#end class
 
-# Helper class, as the current sublimw version does not support the input method yet
-class CallInsertSelectionsplitSnippetCommand(sublime_plugin.TextCommand):
-
-	def run(self, edit, delimiter=None, **args):
-		
-		def handledelimiter(input_string):
-			if is_str_empty(input_string):
-				input_string = get_quicksnippets_setting("default_delimiter", "|")
-			#end if			
-
-			runargs = None			
-			if args != None: # i don't really know why i need to create a copy
-				runargs = dict(args)
-				runargs["delimiter"] = input_string
-			else:
-				runargs=dict(delimiter=input_string)
-			#end if
-
-			sublime.active_window().active_view().run_command("insert_selectionsplit_snippet", runargs)
-		#end def
-
-		if is_str_empty(delimiter):
-			if get_quicksnippets_setting("confirm_default_delimiter", True):			
-				sublime.active_window().show_input_panel("Delimiter?", get_quicksnippets_setting("default_delimiter", "|"), handledelimiter, None, None)
-			else:
-				handledelimiter("")
-			#end if
-		#end if		
+	def input(self, args):
+		if is_str_empty(args.get("delimiter")) and get_quicksnippets_setting("confirm_default_delimiter", True):
+			return InsertSelectionsplitSnippetCommand._DelimiterInputHandler()
+		else:
+			return None
+		#end if
 	#end def
+
 #end class
 
 class RunQuicksnippetCommandCommand(sublime_plugin.ApplicationCommand):
-
 	def castAsDict(self, variables):
 		if variables!=None:		
 			if (type(variables) is dict) != True:
@@ -110,7 +98,7 @@ class RunQuicksnippetCommandCommand(sublime_plugin.ApplicationCommand):
 							elif re.match("(['\"])[^'\"]*?\\1=(['\"])[^'\"]*?\\2", variables) != None:
 								key,val = variables.split("\"=\"",1)
 								variables = sublime.decode_value("{" + key + "\":\""+ val + "}") # may still throw ValueError
-							elif re.match("(['\"])[^'\"]?\\1:(['\"])[^'\"]*?\\2", variables) != None:
+							elif re.match("(['\"])[^'\"]?\\1\\:(['\"])[^'\"]*?\\2", variables) != None:
 								variables = sublime.decode_value("{" + variables + "}") # may still throw ValueError
 							#end if
 						#end try
@@ -130,58 +118,77 @@ class RunQuicksnippetCommandCommand(sublime_plugin.ApplicationCommand):
 		return variables
 	#end def
 
-	def run(self, callbackCommand, contentType, inputPrompt):
-	
-		def runCallbackCommand(runargs):
-			sublime.active_window().active_view().run_command(callbackCommand, runargs)
-		#end def runCallbackCommand
-	
-		def prepare_input(runargs):
-			 
-			def handleInput(input_string):
-				try:
-					finalrunargs=self.castAsDict(input_string)
-					finalrunargs.update(runargs)
-					runCallbackCommand(finalrunargs)
-				except ValueError as e:
-					sublime.error_message(repr(e))
-			#end def handleInput
+	class _SnippetInputHandler(sublime_plugin.ListInputHandler):
+		def __init__(self, child): self.__child = child
+		def name(self): return "name"
+		def description(self, key, value): return value
+		def placeholder(self): return "Choose a snippet"
+		def next_input(self, cmdargs): return self.__child
 		
-			if inputPrompt == True:
-				sublime.active_window().show_input_panel("Enter additional values as JSON keymap", "", handleInput, None, None)				
-			else:
-				runCallbackCommand(runargs)
-			#end if	
-		#end def prepare_input
-		
-		if contentType=='file':
-			
+		def list_items(self):
 			filterpattern = get_quicksnippets_setting("snippet_resource_pattern", "*.sublime-snippet")			
 			filterexp = get_quicksnippets_setting("snippet_filter_expression", None)
 			if filterexp != None:
 				filterexp = re.compile(filterexp,re.IGNORECASE)
 			# end if
 
-			snippet_files = [f for f in sublime.find_resources(filterpattern) if (filterexp == None or filterexp.search(f))]
-			snippets = [list(reversed(os.path.split(f))) for f in snippet_files]
-
-			if (len(snippet_files))>0:
-				def handleSelect(selected_index):
-					if selected_index > -1: # if not canceled
-						prepare_input(dict(name=snippet_files[selected_index]))
-					#end if
-				#end def handleSelect				
-				sublime.active_window().show_quick_panel(snippets, handleSelect)			
-			else:
+			snippets = [(os.path.splitext(os.path.basename(f))[0], f) for f in sublime.find_resources(filterpattern) if (filterexp == None or filterexp.search(f))]
+			if len(snippets)==0:
 				sublime.status_message("No Snippets match the defined filters")
-			#end if	
-			
-		elif contentType=='clipboard':			
-			prepare_input(dict(contents=sublime.get_clipboard()))
-		else:
-			sublime.error_message("Invalid contentType for RunQuicksnippetCommand")
-		#end if
-	
-	#end def run
-#end class
+			return snippets
+		#end def
 
+		def preview(self, text):
+#			if is_str_empty(text): text=get_quicksnippets_setting("default_delimiter", "|")
+			path, file = os.path.split(text)
+			return sublime.Html("{}<br>{}<br><i>{}</i>".format(path, file, "----"))
+		#end def
+	#end sub class
+	
+	class _ArgsInputHandler(sublime_plugin.TextInputHandler):
+		def __init__(self, parentCmd): self.__parentCmd=parentCmd
+		def name(self): return "inputArgs"
+		def description(self, text): return text		
+		def placeholder(self): return "Enter additional values as JSON keymap"
+		def validate(self, text): return self.preview(text) != '--ERROR--'
+		
+		def preview(self, text):
+			try:
+				return sublime.Html("Result: <i>{}</i>".format(str(self.__parentCmd.castAsDict(text))))
+			except:
+				return "--ERROR--"
+		#end def
+	#end sub class
+
+	def run(self, callbackCommand, inputArgs, contents=None, name=None, **args):
+		
+		if contents == "$CLIPBOARD": 
+			contents = sublime.get_clipboard()
+		#end if
+
+		runargs = dict()
+		runargs.update(args)
+		
+		if inputArgs != None:
+			runargs.update(self.castAsDict(inputArgs))
+
+		runargs.update(dict(contents=contents, name=name))
+		sublime.active_window().active_view().run_command(callbackCommand, runargs)
+	#end def
+
+	def input(self, args):
+		handler1 = None
+		handler2 = None
+		
+		if args.get("inputArgs"):
+			handler2 = RunQuicksnippetCommandCommand._ArgsInputHandler(self)
+		#end if
+
+		if is_str_empty(args.get("contents")) and is_str_empty(args.get("name")):
+			handler1 = RunQuicksnippetCommandCommand._SnippetInputHandler(handler2)
+		else:
+			handler1 = handler2
+		#end if
+		
+		return handler1
+	#end def
